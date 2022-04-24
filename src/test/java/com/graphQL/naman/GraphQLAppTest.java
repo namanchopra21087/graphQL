54a59c743dc09c0e6e51ddcc9814843ea9be203a
namanchopra21087 <namanchopra21087@gmail.com>
2022-04-24 20:30:16 +0530

    Initial package commit

---
+++ src/test/java/com/graphQL/naman/GraphQLAppTest.java
@@ -0,0 +1,13 @@
+package com.graphQL.naman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQLAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
